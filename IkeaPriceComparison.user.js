52a7816c6d7d528daa6b919c2573c9f822620fae
Sn0w5t0rm <74139859+Sn0w5t0rm@users.noreply.github.com>
2020-11-09 00:27:33 +0100

    Update IkeaPriceComparison.user.js

--- IkeaPriceComparison.user.js
+++ IkeaPriceComparison.user.js
@@ -9,8 +9,18 @@ window.addEventListener("load", function () {
   function addOtherPrice() {
     const uri = window.location.href;
 
-    const language = "nl/nl"; // Input the country code of the website you wish to compare to in this format '../..'
-    const currency = "€";
+    // Input the country code of the website you wish to compare to in this format '../..'
+    const language = "nl/nl";
+
+    // The base currency used in the country you are comparing to (the one you put in above ^^ )
+    // List of currency codes: https://www.iban.com/currency-codes
+    const baseCurrency = "EUR";
+
+    // The currency you want that article's price to be shown in (the currency you use)
+    const otherCurrency = "USD";
+
+    let exchangeRate = 1;
+    let exchangeRateRequestUri;
 
     const productId = document.getElementsByClassName(
       "range-revamp-product-identifier__number"
@@ -19,40 +29,91 @@ window.addEventListener("load", function () {
     const searchUri = `https://sik.search.blue.cdtapps.com/${language}/search-result-page?&q=${productId}`;
 
     let available = true;
-    let otherPrice = "";
+    let otherPrice;
     let otherSiteUri = "";
 
-    fetch(searchUri)
-      .then(function (result) {
-        return result.json();
-      })
-      .then(function (result) {
-        try {
-          otherPrice = ` ${currency}${result.searchResultPage.productWindow[0].priceNumeral}`;
-          otherSiteUri = result.searchResultPage.productWindow[0].pipUrl;
-        } catch {
-          available = false;
-          otherPrice = " N/A in other country";
-        }
-
-        let otherPriceSpan = document.createElement("span");
-        let otherPricetextnode = document.createTextNode(otherPrice);
-        otherPriceSpan.appendChild(otherPricetextnode);
-        priceSpan[0].appendChild(otherPriceSpan);
-        priceSpan[0].lastChild.style.color = "#a00";
-
-        if (available) {
-          const divToAppend = document.getElementsByClassName(
-            "range-revamp-pip-price-package__price-wrapper"
-          )[0];
-
-          let otherSiteButton = document.createElement("button");
-          otherSiteButton.innerHTML = `<a href="${otherSiteUri}">Take me there</a>`;
-          otherSiteButton.firstChild.style.textDecorationStyle;
-          divToAppend.appendChild(otherSiteButton);
-        }
+    if (
+      baseCurrency !== otherCurrency &&
+      getWithExpiry("exchangeRate") === null
+    ) {
+      exchangeRateRequestUri = `https://api.exchangeratesapi.io/latest?base=${baseCurrency}&symbols=${otherCurrency}`;
+      getExchangeRate().then(function (result) {
+        let helper = result.rates[otherCurrency];
+        helper = helper.toString();
+        setWithExpiry("exchangeRate", helper, 24);
+        lookupPrice();
+        console.log("Fetched exchange rate and updated it in localStorage");
       });
+      console.log("Fetching exchange rate...");
+    } else {
+      lookupPrice();
+    }
+
+    function getExchangeRate() {
+      return fetch(exchangeRateRequestUri).then(function (result) {
+        return result.json();
+      });
+    }
+
+    function setWithExpiry(key, value, ttl) {
+      const now = new Date();
+      const item = {
+        value: value,
+        expiry: now.getTime() + ttl * 1000 * 60 * 60,
+      };
+      localStorage.setItem(key, JSON.stringify(item));
+    }
+
+    function getWithExpiry(key) {
+      const itemStr = localStorage.getItem(key);
+      if (!itemStr) {
+        return null;
+      }
+      const item = JSON.parse(itemStr);
+      const now = new Date();
+      if (now.getTime() > new Date(item.expiry).getTime()) {
+        localStorage.removeItem(key);
+        return null;
+      }
+      return item.value;
+    }
+
+    function lookupPrice() {
+      console.log("Looking up price...");
+      fetch(searchUri)
+        .then(function (result) {
+          return result.json();
+        })
+        .then(function (result) {
+          try {
+            otherPrice = ` ${otherCurrency} ${(
+              result.searchResultPage.productWindow[0].priceNumeral *
+              exchangeRate
+            ).toFixed(2)}`;
+            otherSiteUri = result.searchResultPage.productWindow[0].pipUrl;
+          } catch {
+            available = false;
+            otherPrice = " N/A in other country";
+          }
+
+          let otherPriceSpan = document.createElement("span");
+          let otherPricetextnode = document.createTextNode(otherPrice);
+          otherPriceSpan.appendChild(otherPricetextnode);
+          priceSpan[0].appendChild(otherPriceSpan);
+          priceSpan[0].lastChild.style.color = "#a00";
+
+          if (available) {
+            const divToAppend = document.getElementsByClassName(
+              "range-revamp-pip-price-package__price-wrapper"
+            )[0];
+
+            let otherSiteButton = document.createElement("button");
+            otherSiteButton.innerHTML = `<a href="${otherSiteUri}">Take me there</a>`;
+            otherSiteButton.firstChild.style.textDecorationStyle;
+            divToAppend.appendChild(otherSiteButton);
+          }
+        });
+    }
   }
   addOtherPrice();
 });
-
